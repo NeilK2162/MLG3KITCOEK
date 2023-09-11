c6d692af4ff07259eab7f60fa4d3bae264373cb8
neelambar <kumthekarneelambar@gmail.com>
2023-09-11 12:45:23 +0530

    Updates to the ...

--- schema/paymentSchema.js
+++ schema/paymentSchema.js
@@ -4,27 +4,27 @@ const mongoose = require('mongoose')
 const paymentSchema = new mongoose.Schema({
   payment_method_id: {
     type: String,
-    required: [true, 'Payment ID is required']
+    required: [true, 'Payment ID dya']
   },
   name: {
     type: String,
-    required: [true, 'Name is required']
+    required: [true, 'Name dya']
   },
   email: {
     type: String,
-    required: [true, 'E-mail is required']
+    required: [true, 'E-mail dya']
   },
   amount: {
     type: String,
-    required: [true, 'Amount is required']
+    required: [true, 'Amount dya']
   },
   paymentDate: {
     type: String,
-    required: [true, 'Payment date is required']
+    required: [true, 'Payment dya']
   },
   mobile: {
     type: String,
-    required: [true, 'Mobile number is required']
+    required: [true, 'Mobile number dya']
   }
 
 })
